238c1810b727116d9ed62b1a4db94c3234c81b69
Your Name <emmanuel.montoya@udea.edu.co>
2025-07-24 14:02:13 -0500

    regreso del break

--- codigo/app.js
+++ codigo/app.js
@@ -1,6 +1,10 @@
-angular.module("juegoBingo",[])
-.controller("ControladorJuegoBingo",
-    function($this){
+angular.module("JuegoBingo", [])
+    .controller("ControladorJuegoBingo",
+        function ($scope) {
 
-    }
-);
+            $scope.cantor = new Cantor();
+            $scope.iniciarJuego = function () {
+
+            }
+        }
+    );
