62fcb671875a4d3f11df0ad7c208670aa5ea9c04
emmanuel1901 <emmanuel.montoya@udea.edu.co>
2025-07-28 12:13:22 -0500

    se evitaron numeros repetidos en las tablas

--- codigo/app.js
+++ codigo/app.js
@@ -3,13 +3,33 @@ angular.module("JuegoBingo", [])
         function ($scope) {
 
             $scope.cantor = new Cantor();
+            $scope.jugadores = [];
+            $scope.juegoIniciado = false;
 
             $scope.iniciarJuego = function () {
-
+                const totalJugadores = 7;
+                $scope.jugadores = [];
+                for (let i = 0; i < totalJugadores; i++) {
+                    $scope.jugadores.push({
+                        nombre: "Jugador " + (i + 1),
+                        tablaJugador: new Tabla(),
+                        ganador: false
+                    });
+                }
+                $scope.juegoIniciado = true;
+                $scope.cantor.iniciar();
             }
 
             $scope.sacarBalota = function () {
                 $scope.cantor.sacarBalota();
+
+                for (i = 0; i < $scope.jugadores.length; i++) {
+                    $scope.jugadores[i].tablaJugador.taparNumero($scope.cantor.ultimaBalota);
+                }
+
+                if ($scope.cantor.pendientes == 0) {
+                    $scope.juegoIniciado = false;
+                }
             }
         }
     );
